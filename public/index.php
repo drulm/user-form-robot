42269a0bec0eb10ecb43eda90a9973953a879601
Darrell Ulm <darrellulm@gmail.com>
2017-07-06 18:07:47 -0400

    Some clean up to index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,21 @@
 <?php
 
-// Show all information, defaults to INFO_ALL
-phpinfo();
+// @TODO Error reporting, remove when done.
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
 
-// Show just the module information.
-// phpinfo(8) yields identical results.
-phpinfo(INFO_MODULES);
+// @TODO Clean up.
+require_once '../params/configuration.php';
+require_once '../User/Controller.php';
 
+// Create a new controller instance.
+$controller = new Controller();
 
+// Get the query string.
+// @TODO Do not Access Superglobal $_SERVER Array Directly.
+// @TODO Use some filtering functions instead (e.g. filter_input(), conditions with is_*() functions, etc.).
+$url = $_SERVER['QUERY_STRING'];
+
+// Direct the route given the url information.
+$controller->directRoute($url);
