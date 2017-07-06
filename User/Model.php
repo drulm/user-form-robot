a740373b9ffaeaee408e17f07ba55bfea6bc42bb
Darrell Ulm <darrellulm@gmail.com>
2017-07-06 18:40:10 -0400

    Set model conventions to use for params based on https://tools.ietf.org/html/rfc6570

--- User/Model.php
+++ User/Model.php
@@ -1,8 +1,11 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Note: For parameter names use this convention:
+ * 
+ * id = id
+ * e = email
+ * p = password
+ * fn = first name
+ * ln = last name
  */
-
