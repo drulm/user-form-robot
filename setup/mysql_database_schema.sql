427b9258da5939abc8ffc2017e610e04f9fa6464
Darrell Ulm <darrellulm@gmail.com>
2017-07-07 19:12:04 -0400

    Update database password field to allow hash, with room to grow

--- setup/mysql_database_schema.sql
+++ setup/mysql_database_schema.sql
@@ -7,9 +7,9 @@ CREATE TABLE `users` (
   `email` varchar(254) NOT NULL,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
-  `passwd` varchar(45) NOT NULL,
+  `passwd` varchar(128) NOT NULL,
   PRIMARY KEY (`id_users`),
   UNIQUE KEY `idusers_UNIQUE` (`id_users`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
