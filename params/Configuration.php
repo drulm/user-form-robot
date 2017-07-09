e832122580768cb9288634c7829f990dc7aa078b
Darrell Ulm <darrellulm@gmail.com>
2017-07-09 19:30:59 -0400

    Add error handling to model, clean up code, add comments.

---
+++ params/Configuration.php
@@ -0,0 +1,18 @@
+<?php
+
+class Configuration
+{
+
+    const DB_HOST = '127.0.0.1';
+
+    const DB_MYSQL_PORT = '3306';
+
+    const DB_SCHEMA = 'my_app';
+
+    const DB_USER = 'root';
+
+    const DB_PASSWORD = '86punish';
+    
+    const DEBUG = true;
+
+}
