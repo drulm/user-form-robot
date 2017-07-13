74ccd092224c4054e0847e59e1f6c75ad7904a09
Darrell Ulm <darrellulm@gmail.com>
2017-07-13 12:18:17 -0400

    update fixed title for app

--- User/View.php
+++ User/View.php
@@ -217,7 +217,7 @@ $markup = <<<HTML
 <!DOCTYPE html>
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>php-user-robot</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
